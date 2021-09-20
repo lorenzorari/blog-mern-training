7c61e539b0b8efe6e148a270e4adaf4995886a7c
Lorenzo <lorenzofg24@gmail.com>
2021-09-20 19:13:57 +0200

    Creating Navbar component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  const tabs = ["Home", "Article", "Articles list", "About"];
+
+  return (
+    <header>
+      <nav className="flex justify-center items-center h-16 bg-green-700 text-white">
+        <ul className="flex gap-4">
+          {tabs.map((tab) => (
+            <li className="px-2 py-1 rounded cursor-pointer hover:bg-green-900">
+              {tab}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
