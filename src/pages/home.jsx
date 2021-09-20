5a27eb152be913b52585e3dcb250011b298ca8d9
Lorenzo <lorenzofg24@gmail.com>
2021-09-20 17:57:01 +0200

    Creating HomePage

---
+++ src/pages/home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="mx-80 my-10">
+      <h1 className="mb-6 font-bold text-gray-900 text-2xl sm:text-4xl">
+        Hey, welcome to my blog!
+      </h1>
+
+      <p className="leading-relaxed">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus
+        nihil consequatur rerum, odio similique accusantium nemo ex doloremque,
+        reprehenderit, eligendi fuga dolorum? Mollitia libero error consectetur
+        eligendi? Id, praesentium tempora! Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Accusamus nihil consequatur rerum, odio
+        similique accusantium nemo ex doloremque, reprehenderit, eligendi fuga
+        dolorum? Mollitia libero error consectetur eligendi? Id, praesentium
+        tempora! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Accusamus nihil consequatur rerum, odio similique accusantium nemo ex
+        doloremque, reprehenderit, eligendi fuga dolorum? Mollitia libero error
+        consectetur eligendi? Id, praesentium tempora!
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
