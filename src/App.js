c23dc3686b375b4055348672dc8c925c5375cfd9
Lorenzo <lorenzofg24@gmail.com>
2021-09-20 19:14:17 +0200

    Adding Navbar to layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/navbar";
 import HomePage from "./pages/home";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Route exact path="/" component={HomePage} />
     </BrowserRouter>
   );
