ed82c8273f45c4c6d7675ba3f9871aaff0aac627
Lorenzo <lorenzofg24@gmail.com>
2021-09-22 17:07:06 +0200

    Adding navigation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 const Navbar = () => {
-  const tabs = ["Home", "Articles", "Add an article"];
+  const history = useHistory();
+  const tabs = [
+    { title: "Home", path: "/" },
+    { title: "Articles", path: "/articles" },
+    { title: "Add an article", path: "/add-article" },
+  ];
+
+  const handleClickTab = (tab) => {
+    history.push(tab.path);
+  };
 
   return (
     <header>
       <nav className="flex justify-center items-center h-16 bg-green-700 text-white">
         <ul className="flex gap-4">
-          {tabs.map((tab) => (
-            <li className="px-2 py-1 rounded cursor-pointer hover:bg-green-900">
-              {tab}
+          {tabs.map((tab, i) => (
+            <li
+              key={i}
+              className="px-2 py-1 rounded cursor-pointer hover:bg-green-900"
+              onClick={() => handleClickTab(tab)}
+            >
+              {tab.title}
             </li>
           ))}
         </ul>
