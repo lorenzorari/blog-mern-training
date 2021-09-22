95586ad580d2592e9ad9785910ea503dea453aa4
Lorenzo <lorenzofg24@gmail.com>
2021-09-22 23:29:36 +0200

    Adding classname prop

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-const Article = ({ article }) => {
+const Article = ({ article, className = "" }) => {
   return (
-    <article className="w-80 p-4 bg-gray-100 rounded-md cursor-pointer hover:bg-gray-200">
+    <article
+      className={[
+        "w-80 p-4 bg-gray-100 rounded-md cursor-pointer hover:bg-gray-200",
+        className,
+      ].join(" ")}
+    >
       <h4 className="text-lg font-bold">{article.title}</h4>
       <p className="text-gray-500">by {article.author}</p>
     </article>
